bebbacf607ac63d1f84a84b7b305378fd050c336
mdo <michael.doswald@schiller.ch>
2023-05-23 13:13:29 +0200

    Added support for job cancellation - added cancel() method to IppJob - added cancelCurrentJob() method to IppJob - added getJobId() to IppJob to return the job-id as used by the printer

--- src/main/java/jipsi/de/lohndirekt/print/api/IppJob.java
+++ src/main/java/jipsi/de/lohndirekt/print/api/IppJob.java
@@ -15,6 +15,7 @@ import jipsi.de.lohndirekt.print.IppRequestFactory;
 import jipsi.de.lohndirekt.print.IppResponse;
 import jipsi.de.lohndirekt.print.attribute.AttributeMap;
 import jipsi.de.lohndirekt.print.attribute.IppAttributeName;
+import jipsi.de.lohndirekt.print.attribute.IppStatus;
 import jipsi.de.lohndirekt.print.attribute.ipp.jobdesc.JobId;
 import jipsi.de.lohndirekt.print.attribute.ipp.jobdesc.JobUri;
 import jipsi.de.lohndirekt.print.attribute.ipp.printerdesc.supported.OperationsSupported;
@@ -42,6 +43,11 @@ public abstract class IppJob
     if (jobId == null && jobUri == null) {
       throw new IllegalArgumentException("JobId or JobUri must be available");
     }
+  }
+  
+  public JobId getJobId()
+  {
+    return jobId;
   }
 
   public JobState getStatus()
@@ -72,6 +78,63 @@ public abstract class IppJob
     return JobState.UNKNOWN;
   }
 
+  /**
+   *  Send a {@link OperationsSupported#CANCEL_JOB} command to the printer
+   * to cancel the job that is represented by this job object.
+   * 
+   * @return {@code true} if the printer returns a success message
+   */
+  public boolean cancel()
+  {
+    IppRequest cancelJobRequest = IppRequestFactory.createIppRequest(printer.getUri(), OperationsSupported.CANCEL_JOB, printer.getRequestingUserName(), null);
+    addJobReference(cancelJobRequest);
+
+    try {
+      IppResponse response = cancelJobRequest.send();
+
+      if (response.getStatus().equals(IppStatus.SUCCESSFUL_OK)
+          || response.getStatus().equals(IppStatus.SUCCESSFUL_OK_CONFLICTING_ATTRIBUTES)
+          || response.getStatus().equals(IppStatus.SUCCESSFUL_OK_CONFLICTING_ATTRIBUTES)) {
+        return true;
+      }
+    }
+    catch (IOException ex) {
+      LOG.error(ex.getMessage(), ex);
+    }
+    
+    return false;
+  }
+  
+  /**
+   * Send a {@link OperationsSupported#CANCEL_CURRENT_JOB} command to the printer
+   * to cancel the current job. Note that this command will send the job-id of
+   * this job to make sure that the current job is the job represented by this
+   * job object.
+   * 
+   * @return {@code true} if the printer returns a success message
+   */
+  public boolean cancelCurrentJob()
+  {
+    IppRequest cancelCurrentJobRequest = IppRequestFactory.createIppRequest(printer.getUri(), OperationsSupported.CANCEL_CURRENT_JOB, printer.getRequestingUserName(), null);
+    addJobReference(cancelCurrentJobRequest);
+
+    try {
+      IppResponse response = cancelCurrentJobRequest.send();
+
+      if (response.getStatus().equals(IppStatus.SUCCESSFUL_OK)
+          || response.getStatus().equals(IppStatus.SUCCESSFUL_OK_CONFLICTING_ATTRIBUTES)
+          || response.getStatus().equals(IppStatus.SUCCESSFUL_OK_CONFLICTING_ATTRIBUTES)) {
+        return true;
+      }
+    }
+    catch (IOException ex) {
+      LOG.error(ex.getMessage(), ex);
+    }
+    
+    return false;
+  }
+  
+  
   private void addJobReference(IppRequest request)
   {
     if (jobId != null) {
